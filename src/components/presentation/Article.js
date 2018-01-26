008ac2ea95f484da8b2a5a39caf03df6b9ee17e2
Michael Byrne <mpbyrne@gmail.com>
2018-01-26 15:01:13 -0800

    pared down article comp

--- src/components/presentation/Article.js
+++ src/components/presentation/Article.js
@@ -1,36 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 
-class Article extends Component {
-  constructor(props){
-    super(props);
-
-    this.state = {expanded: props.selected};
+const Article = ({selected,handleSelect,id,title,author,text}) => {
+  if(selected === false) {
+    return (
+      <div className="article" onClick={handleSelect} data-id={id}>
+        <div className="topic-name">{title}</div>
+        <div className="topic-num">Written by <span className="author-name">{author}</span></div>
+        <div className="topic-author">Updated over a week ago</div>
+      </div>
+    )
   }
-
-
-  render(){
-    if(this.props.selected === false) {
-      return (
-        <div className="article" onClick={this.props.handleSelect} data-id={this.props.id}>
-          <div className="topic-name">{this.props.title}</div>
-          <div className="topic-num">Written by <span className="author-name">{this.props.author}</span></div>
-          <div className="topic-author">Updated over a week ago</div>
-        </div>
-      )
-    }
-    else{
-      return(
-        <div className="article" onClick={this.props.handleSelect} data-id={this.props.id}>
-          <div className="topic-name">{this.props.title}</div>
-          <div className="topic-num">Written by <span className="author-name">{this.props.author}</span></div>
-          <div className="topic-author">Updated over a week ago</div>
-          <div className="article-text">{this.props.text}</div>
-        </div>
-      )
-    }
+  else{
+    return(
+      <div className="article" onClick={handleSelect} data-id={id}>
+        <div className="topic-name">{title}</div>
+        <div className="topic-num">Written by <span className="author-name">{author}</span></div>
+        <div className="topic-author">Updated over a week ago</div>
+        <div className="article-text">{text}</div>
+      </div>
+    )
   }
 }
-
 
 export default Article;
